41a9f6e5e9046c33ab397a8928ed596d4a7e584f
sanchittanwar7 <tanwarsanchit9762@gmail.com>
2017-11-05 01:51:30 +0530

    notifications updated

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,12 @@
 class NotificationsController < ApplicationController
-  def show
-  end
-  def check
-  end
+	def show
+		@notifications = Notification.all.order('created_at desc')
+	end
+	def check
+		@newNotification = Notification.where(receiver_id: current_user.id, read: false)[0]
+		if @newNotification == nil
+			render :nothing => true, :status => :ok
+			return true
+		end
+	end
 end
